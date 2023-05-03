dbe895f1262165df31e352a2ada1854df8873eb9
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-03 11:17:13 +0530

    datatable code formating

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,39 +1,46 @@
-import './datatable.scss';
-import { DataGrid } from '@mui/x-data-grid';
-import { userRows,userColumns } from '../../datatablesource';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import "./datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { userRows, userColumns } from "../../datatablesource";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Datatable = () => {
-    const [data,setData]=useState(userRows);
-    const handleDelete = (id) => { 
-      setData(data.filter(item=>item.id!==id));  
-     }
-    const actionColumn = [
-        {field:'action',headerName:"Action",width:200,renderCell:(params)=>{
-            return (
-                <div className="cellAction">
-                            <Link to="/users/test" style={{textDecoration:'none'}}>
-                    <div className="viewButton">
-                        View
-                    </div>
-                    </Link>
-                    <div className="deleteButton" onClick={()=>handleDelete(params.row.id)}>
-                        Delete
-                    </div>
-                </div>
-            )
-        }}
-    ]
+  const [data, setData] = useState(userRows);
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
+            <div
+              className="deleteButton"
+              onClick={() => handleDelete(params.row.id)}
+            >
+              Delete
+            </div>
+          </div>
+        );
+      },
+    },
+  ];
   return (
-    <div className='datatable'>
-        <div className="datatableTitle">
-            Add New User 
-            <Link to="new" className='link'>
-Add New</Link>
-        </div>
-        <DataGrid
-        className='datagrid'
+    <div className="datatable">
+      <div className="datatableTitle">
+        Add New User
+        <Link to="new" className="link">
+          Add New
+        </Link>
+      </div>
+      <DataGrid
+        className="datagrid"
         rows={data}
         columns={userColumns.concat(actionColumn)}
         pageSize={8}
@@ -41,7 +48,7 @@ Add New</Link>
         checkboxSelection
       />
     </div>
-  )
-}
+  );
+};
 
-export default Datatable
+export default Datatable;
