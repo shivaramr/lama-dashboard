05c26c725174a8579e85775939a98fb1e642cc3e
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-03 11:19:49 +0530

    navbar code formating

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,55 +1,62 @@
-import { memo, useContext } from 'react'
-import './navbar.scss'
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import { memo, useContext } from "react";
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
-import { DarkModeContext } from '../../context/darkModeContext';
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Navbar = memo(() => {
-  const {darkMode,dispatch}=useContext(DarkModeContext);
+  const { darkMode, dispatch } = useContext(DarkModeContext);
 
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="wrapper">
         <div className="search">
-          <input type="text" placeholder='Search...' />
-          <SearchOutlinedIcon/>
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon />
         </div>
         <div className="items">
           <div className="item">
-             <LanguageOutlinedIcon className='icon'/>
-             English
+            <LanguageOutlinedIcon className="icon" />
+            English
           </div>
-          <div className="item" onClick={()=>dispatch({type:"TOGGLE"})}>
-            {darkMode? <WbSunnyIcon className='icon' />:<DarkModeOutlinedIcon className='icon'  />}
+          <div className="item" onClick={() => dispatch({ type: "TOGGLE" })}>
+            {darkMode ? (
+              <WbSunnyIcon className="icon" />
+            ) : (
+              <DarkModeOutlinedIcon className="icon" />
+            )}
           </div>
           <div className="item">
-            <FullscreenExitOutlinedIcon className='icon' />
+            <FullscreenExitOutlinedIcon className="icon" />
           </div>
           <div className="item">
-            <NotificationsNoneOutlinedIcon className='icon' />
+            <NotificationsNoneOutlinedIcon className="icon" />
             <div className="counter">1</div>
           </div>
           <div className="item">
-            <ChatBubbleOutlineOutlinedIcon className='icon' />
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
             <div className="counter">2</div>
           </div>
           <div className="item">
-            <ListOutlinedIcon className='icon' />
+            <ListOutlinedIcon className="icon" />
           </div>
           <div className="item">
-            <img src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
-            alt="avatar" className='avatar' />
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt="avatar"
+              className="avatar"
+            />
           </div>
         </div>
       </div>
     </div>
-  )
-})
+  );
+});
 
-export default Navbar
+export default Navbar;
