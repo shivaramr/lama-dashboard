2ab70541460439753c7eca00fe0a2c6f96ff69ba
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-05 09:52:13 +0530

    context code formating

--- src/context/darkModeReducer.js
+++ src/context/darkModeReducer.js
@@ -1,16 +1,16 @@
-const DarkModeReducer = (state,action) => {
-    switch(action.type){
-        case "LIGHT": {
-            return {darkMode: false}
-        }
-        case "DARK": {
-            return {darkMode: true}
-        }
-        case "TOGGLE": {
-            return {darkMode: !state.darkMode}
-        }
-        default:
-            return state;
+const DarkModeReducer = (state, action) => {
+  switch (action.type) {
+    case "LIGHT": {
+      return { darkMode: false };
     }
-}
+    case "DARK": {
+      return { darkMode: true };
+    }
+    case "TOGGLE": {
+      return { darkMode: !state.darkMode };
+    }
+    default:
+      return state;
+  }
+};
 export default DarkModeReducer;
