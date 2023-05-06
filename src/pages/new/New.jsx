6f05d438a4de842b6abdcf58f3203ea22573ea2c
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-06 17:19:52 +0530

    new code formating

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,13 +1,13 @@
-import './new.scss'
-import Sidebar from '../../components/sidebar/Sidebar';
-import Navbar from '../../components/navbar/Navbar';
-import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
-import { useState } from 'react';
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import { useState } from "react";
 
-const New = ({inputs,title}) => {
-  const[file,setFile]=useState("");
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
   return (
-    <div className='new'>
+    <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
@@ -16,23 +16,35 @@ const New = ({inputs,title}) => {
         </div>
         <div className="bottom">
           <div className="left">
-            <img 
-            src={file?URL.createObjectURL(file):"https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
-            alt="" />
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
           </div>
           <div className="right">
             <form>
               <div className="formInput">
-                <label htmlFor='file'>
-                Image: <DriveFolderUploadOutlinedIcon className='icon' />
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
                 </label>
-                <input type='file' id="file" onChange={e=>setFile(e.target.files[0])} style={{display:'none'}} />
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
               </div>
 
-              {inputs.map(input=>(<div className="formInput" key={input.id}>
-              <label>{input.label}</label>
-                <input type={input.type} placeholder={input.placeholder} />
-              </div>))}
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
 
               <button>Send</button>
             </form>
@@ -40,7 +52,7 @@ const New = ({inputs,title}) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default New
+export default New;
