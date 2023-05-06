8e4a11bf971fd66984a9a605f9b23b318dec0022
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-06 17:17:47 +0530

    list code formating

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,18 +1,18 @@
-import Datatable from '../../components/datatable/Datatable'
-import Navbar from '../../components/navbar/Navbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import './list.scss'
+import Datatable from "../../components/datatable/Datatable";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./list.scss";
 
 const List = () => {
   return (
-    <div className='list'>
+    <div className="list">
       <Sidebar />
       <div className="listContainer">
         <Navbar />
         <Datatable />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
